559dbb869d09348d6c331910e8953a2aeee183e0
ErickaV17 <ericka.vandongen611@sccdsb.net>
2020-09-23 08:28:09 -0400

    Update AboutMe.java

--- AboutMe.java
+++ AboutMe.java
@@ -7,7 +7,7 @@
 public class AboutMe {
     public static void main (String [] args) {
         // Declaring the variables
-        String myName = "Ericka";
+        String myName = "Ericka Van Dongen";
         int myAge = 15 ;
         double myHeightInMetres = 1.65 ;
         
